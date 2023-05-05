639b81620bf4c763ec3ded525d904394f728854f
Riley <riley@kjhalloran.com>
2023-05-05 01:45:25 -0400

    adding the first pass of the paper

--- roberta1.py
+++ roberta1.py
@@ -2,14 +2,22 @@
 import sys
 import pandas as pd
 import numpy as np
+import tensorflow as tf
 import transformers
-from transformers import RobertaModel, RobertaTokenizer
+from transformers import RobertaModel, RobertaConfig, RobertaTokenizer
 
 if __name__ == '__main__':
-    if(len(sys.argv) < 1):
+    if(len(sys.argv) < 2):
         raise Exception("error")
     
+    #import the datasets as strings
     training_str = sys.argv[1]
+    testing_str = sys.argv[2]
 
     training_df = pd.read_json(training_str).T
-    print(training_df.head())
+    testing_df = pd.read_json(training_str).T
+
+    configuration = RobertaConfig()
+    model = RobertaModel(configuration)
+
+    print(testing_df.head())
